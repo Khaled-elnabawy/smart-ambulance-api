34ef44b72801e380a86f60303891feff6f1a9f53
Amr Saleh <amrsalehma@gmail.com>
2026-03-07 02:35:58 +0200

    added networking layer with dio, retrofit, and data & logic layer in login feature

---
+++ mobile/lib/features/login/data/models/login_response.dart
@@ -0,0 +1,56 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'login_response.g.dart';
+
+@JsonSerializable()
+class LoginResponse {
+  String? message;
+  @JsonKey(name: 'data')
+  UserData? userData;
+  bool? status;
+  int? code;
+
+  LoginResponse({this.message, this.userData, this.status, this.code});
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) =>
+      _$LoginResponseFromJson(json);
+}
+
+@JsonSerializable()
+class UserData {
+  String? token;
+  @JsonKey(name: 'user')
+  UserInfo? userInfo;
+
+  UserData({this.token, this.userInfo});
+
+  factory UserData.fromJson(Map<String, dynamic> json) =>
+      _$UserDataFromJson(json);
+}
+
+@JsonSerializable()
+class UserInfo {
+  int? id;
+  String? name;
+  String? phone;
+  String? email;
+  @JsonKey(name: 'created_at')
+  String? createdAt;
+  @JsonKey(name: 'updated_at')
+  String? updatedAt;
+  @JsonKey(name: 'deleted_at')
+  String? deletedAt;
+
+  UserInfo({
+    this.id,
+    this.name,
+    this.phone,
+    this.email,
+    this.createdAt,
+    this.updatedAt,
+    this.deletedAt,
+  });
+
+  factory UserInfo.fromJson(Map<String, dynamic> json) =>
+      _$UserInfoFromJson(json);
+}
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/core/di/dependency_injection.dart';
 import 'package:mobile/core/routing/app_router.dart';
 import 'package:mobile/smart_ambulance.dart';
 
 void main() {
+  setupGetIt();
   runApp(SmartAmbulance(appRouter: AppRouter()));
 }
