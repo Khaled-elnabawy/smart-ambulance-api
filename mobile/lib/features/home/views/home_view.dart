c89cb5ce9d8303ac2682f2432e4b18504c1a1770
Amr Saleh <amrsalehma@gmail.com>
2026-03-25 22:59:18 +0200

    convert home view to stateful widget and add google map controller and markers

--- mobile/lib/features/home/views/home_view.dart
+++ mobile/lib/features/home/views/home_view.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  late final CameraPosition initialCameraPosition;
+  late GoogleMapController _mapController;
+  Set<Marker> markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    initialCameraPosition = CameraPosition(
+      target: LatLng(31.04425054350228, 31.363826542206063),
+      zoom: 16.0,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(target: LatLng(37.42796133580664, -122.085749655962)),
+        markers: markers,
+        initialCameraPosition: initialCameraPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _mapController = controller;
+        },
       ),
     );
   }
