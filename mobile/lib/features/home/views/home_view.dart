1af7e125051f5eb2d779afb3ce0d39f5be36dbd9
Amr Saleh <amrsalehma@gmail.com>
2026-03-26 16:35:08 +0200

    added custom markers to home map, added marker assets and adjusted map zoom settings

--- mobile/lib/features/home/views/home_view.dart
+++ mobile/lib/features/home/views/home_view.dart
@@ -18,8 +18,44 @@ class _HomeViewState extends State<HomeView> {
     super.initState();
     initialCameraPosition = CameraPosition(
       target: LatLng(31.04425054350228, 31.363826542206063),
-      zoom: 16.0,
+      zoom: 15.5,
     );
+    initMarkers();
+  }
+
+  /*Future<Uint8List> getImageFromRawData(String image, double width) async {
+    var imageData = await rootBundle.load(image);
+    var imageCodec = await ui.instantiateImageCodec(
+        imageData.buffer.asUint8List(),
+        targetWidth: width.round());
+
+    var imageFrameInfo = await imageCodec.getNextFrame();
+
+    var imageBytData =
+        await imageFrameInfo.image.toByteData(format: ui.ImageByteFormat.png);
+
+    return imageBytData!.buffer.asUint8List();
+  }*/
+
+  void initMarkers() async {
+    Marker help = Marker(
+      markerId: MarkerId('1'),
+      icon: await BitmapDescriptor.asset(
+        ImageConfiguration.empty,
+        'assets/images/help_point.png',
+      ),
+      position: LatLng(31.046162023854304, 31.365445177935857),
+    );
+    Marker ambulancer = Marker(
+      markerId: MarkerId('2'),
+      icon: await BitmapDescriptor.asset(
+        ImageConfiguration.empty,
+        'assets/images/truck_kun.png',
+      ),
+      position: LatLng(31.04421054551063, 31.36439857355452),
+    );
+    markers.add(help);
+    markers.add(ambulancer);
   }
 
   @override
@@ -31,6 +67,8 @@ class _HomeViewState extends State<HomeView> {
         onMapCreated: (GoogleMapController controller) {
           _mapController = controller;
         },
+        zoomControlsEnabled: false,
+
       ),
     );
   }
