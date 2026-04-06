cf128077974732afa83301c8cb5608d6dde5109e
Amr Saleh <amrsalehma@gmail.com>
2026-04-06 23:29:59 +0200

    reformatted navigation extensions for better readability and updated pop method to support optional arguments

--- mobile/lib/core/helpers/extensions.dart
+++ mobile/lib/core/helpers/extensions.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 extension Navigation on BuildContext {
   Future<dynamic> pushNamed(String routeName, {Object? arguments}) {
-    return Navigator.of(this,rootNavigator: true).pushNamed(routeName, arguments: arguments);
+    return Navigator.of(
+      this,
+      rootNavigator: true,
+    ).pushNamed(routeName, arguments: arguments);
   }
 
   Future<dynamic> pushReplacementNamed(String routeName, {Object? arguments}) {
     return Navigator.of(
-      this,rootNavigator: true
+      this,
+      rootNavigator: true,
     ).pushReplacementNamed(routeName, arguments: arguments);
   }
 
@@ -17,9 +21,11 @@ extension Navigation on BuildContext {
     required RoutePredicate predicate,
   }) {
     return Navigator.of(
-      this,rootNavigator: true
+      this,
+      rootNavigator: true,
     ).pushNamedAndRemoveUntil(routeName, predicate, arguments: arguments);
   }
 
-  void pop() => Navigator.of(this,rootNavigator: true).pop();
- }
+  void pop([Object? argument]) =>
+      Navigator.of(this, rootNavigator: true).pop(argument);
+}
