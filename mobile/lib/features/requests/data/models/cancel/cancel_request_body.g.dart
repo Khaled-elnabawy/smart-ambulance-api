4823ed8524d0fc4fe535d2c7cd441226c357951c
Amr Saleh <amrsalehma@gmail.com>
2026-04-11 00:02:32 +0200

    added request and response models for request cancellation

---
+++ mobile/lib/features/requests/data/models/cancel/cancel_request_body.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cancel_request_body.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CancelRequestBody _$CancelRequestBodyFromJson(Map<String, dynamic> json) =>
+    CancelRequestBody(id: (json['id'] as num?)?.toInt());
+
+Map<String, dynamic> _$CancelRequestBodyToJson(CancelRequestBody instance) =>
+    <String, dynamic>{'id': instance.id};
