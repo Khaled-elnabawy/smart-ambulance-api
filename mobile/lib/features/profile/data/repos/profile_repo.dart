bba909bdc788dd9686663878161314a9e6786a10
Amr Saleh <amrsalehma@gmail.com>
2026-04-14 16:21:16 +0200

    integrate edit profile fun in profile repo

--- mobile/lib/features/profile/data/repos/profile_repo.dart
+++ mobile/lib/features/profile/data/repos/profile_repo.dart
@@ -1,7 +1,9 @@
 import 'package:mobile/core/networking/api_result.dart';
 import 'package:mobile/core/networking/api_service.dart';
+import 'package:mobile/features/profile/data/models/edit_profile/edit_profile_response_model.dart';
 import 'package:mobile/features/profile/data/models/logout/logout_response_model.dart';
 import '../../../../core/networking/api_error_handling.dart';
+import '../models/edit_profile/edit_profile_request_model.dart';
 
 class ProfileRepo {
   final ApiService _apiService;
@@ -11,6 +13,21 @@ class ProfileRepo {
   Future<ApiResult<LogoutResponseModel>> logout(String token) async {
     try {
       final response = await _apiService.logout('Bearer $token');
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+
+  Future<ApiResult<EditProfileResponseModel>> editProfile(
+    String token,
+    EditProfileRequestModel editProfileRequestModel,
+  ) async {
+    try {
+      final response = await _apiService.editProfile(
+        'Bearer $token',
+        editProfileRequestModel,
+      );
       return ApiResult.success(response);
     } catch (error) {
       return ApiResult.failure(ErrorHandler.handle(error));
