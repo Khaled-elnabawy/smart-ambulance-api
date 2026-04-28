f5bd6fe99ffc2614c97969b00bd0ed1ca3504440
Amr Saleh <amrsalehma@gmail.com>
2026-04-28 03:10:01 +0300

    wrapped BlocListener around requests view to auto-refresh lists on navigation, reorganized imports, and removed unused dependencies.

--- mobile/lib/features/requests/views/requests_view.dart
+++ mobile/lib/features/requests/views/requests_view.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mobile/core/helpers/spacing.dart';
-import 'package:mobile/features/requests/views/widgets/cancel_bloc_listener.dart';
-import 'package:mobile/features/requests/views/widgets/confirm_bloc_listener.dart';
 import 'package:mobile/features/requests/views/widgets/emergency_requests_bloc_listener.dart';
-import 'package:mobile/features/requests/views/widgets/reject_bloc_listener.dart';
 import 'package:mobile/features/requests/views/widgets/scheduled_bloc_listener.dart';
 import 'package:mobile/features/requests/views/widgets/scheduled_request_widget.dart';
+import 'package:mobile/features/requests/views/widgets/cancel_bloc_listener.dart';
+import 'package:mobile/features/requests/views/widgets/confirm_bloc_listener.dart';
+import 'package:mobile/features/requests/views/widgets/reject_bloc_listener.dart';
 import 'package:mobile/features/requests/views/widgets/sos_request_widget.dart';
 import 'package:mobile/features/requests/views/widgets/toggle_widget.dart';
-import '../../../core/di/dependency_injection.dart';
 import '../../../core/theming/styles.dart';
-import '../../../core/widgets/back_button_widget.dart';
-import '../data/repo/requests_repo.dart';
+import '../../../layouts/main/cubit/bottom_nav_cubit.dart';
 import '../logic/requests_cubits/emergency_requests_cubit.dart';
 import '../logic/requests_cubits/scheduled_requests_cubit.dart';
 
@@ -43,44 +40,52 @@ class _RequestsViewState extends State<RequestsView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            verticalSpacing(82),
-            Text('Requests', style: TextStyles.font30BlackSemiBold),
-            verticalSpacing(24),
-            ToggleWidget(
-              onChange: (value) {
-                setState(() {
-                  isSOS = value;
-                });
-              },
-            ),
-            verticalSpacing(20),
-            Expanded(
-              child: isSOS
-                  ? SosRequestWidget(
-                      token: widget.token ?? '',
-                      isDriver: widget.isDriver,
-                    )
-                  : ScheduledRequestWidget(
-                      token: widget.token ?? '',
-                      isDriver: widget.isDriver,
-                    ),
-            ),
-            EmergencyRequestsBlocListener(),
-            ScheduledRequestsBlocListener(),
-            CancelBlocListener(onSuccess: _refreshActiveList),
-            if (widget.isDriver) ...[
-              ConfirmBlocListener(onSuccess: _refreshActiveList),
-              RejectBlocListener(onSuccess: _refreshActiveList),
+    return BlocListener<BottomNavCubit, int>(
+      listener: (context, state) {
+        if (state == 1) {
+          _refreshActiveList();
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              verticalSpacing(82),
+              Text('Requests', style: TextStyles.font30BlackSemiBold),
+              verticalSpacing(24),
+              ToggleWidget(
+                onChange: (value) {
+                  setState(() {
+                    isSOS = value;
+                  });
+                },
+              ),
+              verticalSpacing(20),
+              Expanded(
+                child: isSOS
+                    ? SosRequestWidget(
+                        token: widget.token ?? '',
+                        isDriver: widget.isDriver,
+                      )
+                    : ScheduledRequestWidget(
+                        token: widget.token ?? '',
+                        isDriver: widget.isDriver,
+                      ),
+              ),
+              EmergencyRequestsBlocListener(),
+              ScheduledRequestsBlocListener(),
+              CancelBlocListener(onSuccess: _refreshActiveList),
+              if (widget.isDriver) ...[
+                ConfirmBlocListener(onSuccess: _refreshActiveList),
+                RejectBlocListener(onSuccess: _refreshActiveList),
+              ],
             ],
-          ],
+          ),
         ),
       ),
     );
   }
+
   void _refreshActiveList() {
     if (isSOS) {
       context.read<EmergencyRequestsCubit>().emitEmergencyState(
